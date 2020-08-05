3c3612aafbcc83c4986c9c051fd8d4e6d9f8c66b
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-08-05 16:23:58 +0200

    Feature - add Spy logger to Testing.Logging (#23)

---
+++ docs/preview/index.md
@@ -0,0 +1,28 @@
+---
+title: "Home"
+layout: default
+permalink: /
+redirect_from:
+ - /index.html
+---
+
+[![NuGet Badge](https://buildstats.info/nuget/Arcus.Testing.Logging?includePreReleases=true)](https://www.nuget.org/packages/Arcus.Testing.Logging/)
+
+# Installation
+
+The Arcus.Testing.Logging can be installed via NuGet:
+
+```shell
+PM > Install-Package Arcus.Testing.Logging
+```
+
+For more granular packages we recommend reading the documentation.
+
+# Features
+
+- [xUnit Logging and Logging Testing](logging)
+
+# License
+This is licensed under The MIT License (MIT). Which means that you can use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the web application. But you always need to state that Codit is the original author of this web application.
+
+*[Full license here](https://github.com/arcus-azure/arcus.testing/blob/master/LICENSE)*
---
+++ src/Arcus.Testing.Logging/InMemoryLogger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using GuardNet;
+using Microsoft.Extensions.Logging;
+
+namespace Arcus.Testing.Logging
+{
+    /// <summary>
+    /// Spy (stub) <see cref="ILogger"/> implementation to track the logged messages in-memory.
+    /// </summary>
+    public class InMemoryLogger : ILogger
+    {
+        private readonly ConcurrentBag<LogEntry> _entries = new ConcurrentBag<LogEntry>();
+
+        /// <summary>
+        /// Gets the current logged messages.
+        /// </summary>
+        public IEnumerable<string> Messages => _entries.Select(e => e.Message).Reverse();
+
+        /// <summary>
+        /// Gets the current logged entries.
+        /// </summary>
+        public IEnumerable<LogEntry> Entries => _entries.AsEnumerable().Reverse();
+
+        /// <summary>Writes a log entry.</summary>
+        /// <param name="logLevel">Entry will be written on this level.</param>
+        /// <param name="eventId">Id of the event.</param>
+        /// <param name="state">The entry to be written. Can be also an object.</param>
+        /// <param name="exception">The exception related to this entry.</param>
+        /// <param name="formatter">Function to create a <see cref="T:System.String" /> message of the <paramref name="state" /> and <paramref name="exception" />.</param>
+        /// <typeparam name="TState">The type of the object to be written.</typeparam>
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            Guard.NotNull(formatter, nameof(formatter));
+
+            string message = formatter(state, exception);
+
+            var entry = new LogEntry(eventId, logLevel, message, exception);
+            _entries.Add(entry);
+        }
+
+        /// <summary>
+        /// Checks if the given <paramref name="logLevel" /> is enabled.
+        /// </summary>
+        /// <param name="logLevel">level to be checked.</param>
+        /// <returns><c>true</c> if enabled.</returns>
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        /// <summary>Begins a logical operation scope.</summary>
+        /// <param name="state">The identifier for the scope.</param>
+        /// <typeparam name="TState">The type of the state to begin scope for.</typeparam>
+        /// <returns>An <see cref="T:System.IDisposable" /> that ends the logical operation scope on dispose.</returns>
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+}
