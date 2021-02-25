8a548a30c096d13373508083d3101edc3d2a40cc
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-02-25 10:11:58 +0100

    Feature - add in-memory secret provider for the Arcus secret store (#35)

---
+++ docs/preview/features/inmemory-secret-provider.md
@@ -0,0 +1,96 @@
+---
+title: Security
+layout: default
+---
+
+# Security
+
+## Installation
+
+The following functionality is available when installing this package:
+
+```shell
+PM> Install-Package -Name Arcus.Testing.Security.Providers.InMemory
+```
+
+## In-memory secret provider
+
+As an addition to the [Arcus Security]() package, we have added an in-memory `ISecretProvider` implementation. 
+This secret provider created so you can test you secret store configuration with non-secret values in a easy manner, without implementing your own `ISecretProvider`.
+
+After installing the package, the `.AddInMemory` extension should be available to you:
+
+```csharp
+using Microsoft.Extensions.Hosting;
+
+public void Program
+{
+    public static void Main(string[] args)
+    {
+        Host.CreateDefaultBuilder()
+            .ConfigureSecretStore((config, stores) =>
+            {
+                // Adding a in-memory secret provider to the secret store, without any additional secrets.
+                // This is mainly used to have at least a single secret provider registration which is required for the secret store to be set up.
+                stores.AddInMemory();
+
+                // Adding a in-memory secret provider to the secret store, with a single secret name/value pair.
+                stores.AddInMemory("MySecret", "P@ssw0rd");
+
+                // Adding a in-memory secret provider to the secret store, with several secret name/value pairs.
+                stores.AddInMemory(new Dictionary<string, string>
+                {
+                    ["MySecret-1"] = "P@ssw0rd",
+                    ["MySecret-2"] = "qwerty"
+                });
+            })
+            .Build()
+            .Run();
+    }
+}
+```
+
+The secret store will behave the same, so this in-memory secret provider will be a part when you inject the `ISecretProvider` in your application:
+
+```csharp
+using Arcus.Security.Core;
+
+[ApiController]
+public class MyController : ControllerBase
+{
+    public MyController(ISecretProvider secretProvider)
+    {
+        secretProvider.GetRawSecretAsync("MySecret");
+    }
+}
+```
+
+### Customization
+
+The in-memory secret provider also has some several extra options to customize the usage.
+
+```csharp
+using Microsoft.Extenions.Hosting;
+
+public void Program
+{
+    public static void Main(string[] args)
+    {
+        Host.CreateDefaultBuilder()
+            .ConfigureSecretStore((config, stores) =>
+            {
+                // Adding a in-memory secret provider to the secret store, with caching configuration.
+                // This means that the secret provider will be registered as a cached variant and can be retrieved as such (via `ISecretStore.GetCachedProvider`).
+                // For more information on caching secrets: https://security.arcus-azure.net/features/secrets/general
+                stores.AddInMemory("MySecret", "P@ssw0rd", new CacheConfiguration(TimeSpan.FromSeconds(5));
+
+                // Adding a in-memory secret provider to the secret store, with a dedicated name.
+                // This means that the secret provider can be retrieved with the `ISecretStore.GetProvider("your-name")`.
+                // For more information on retrieving a specific secret provider: https://security.arcus-azure.net/features/secret-store/named-secret-providers
+                stores.AddInMemory(new Dictionary<string, string> { ["MySecret"] = "P@ssw0rd" }, secretProviderName: "InMemory");
+            })
+            .Build()
+            .Run();
+    }
+}
+```
---
+++ src/Arcus.Testing.Security.Providers.InMemory/Extensions/SecretStoreBuilderExtensions.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using Arcus.Security.Core.Caching.Configuration;
+using Arcus.Testing.Security.Providers.InMemory;
+using GuardNet;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Hosting
+{
+    /// <summary>
+    /// Extensions on the <see cref="SecretStoreBuilder"/> class to add test-related options.
+    /// </summary>
+    public static class SecretStoreBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="InMemorySecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+
+            builder.AddInMemory(secretProviderName: null);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemoryCachedSecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="cacheConfiguration">The <see cref="ICacheConfiguration" /> which defines how the cache works.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder, ICacheConfiguration cacheConfiguration)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            
+            builder.AddInMemory(cacheConfiguration, secretProviderName: null);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="InMemorySecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secretProviderName">The name to register the secret provider by.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder, string secretProviderName)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+
+            builder.AddProvider(new InMemorySecretProvider(), options => options.Name = secretProviderName);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="InMemoryCachedSecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="cacheConfiguration">The <see cref="ICacheConfiguration" /> which defines how the cache works.</param>
+        /// <param name="secretProviderName">The name to register the secret provider by.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or the <paramref name="cacheConfiguration"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder, ICacheConfiguration cacheConfiguration, string secretProviderName)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNull(cacheConfiguration, nameof(cacheConfiguration), "Requires a configuration instance to describe how the caching of secrets should work");
+            
+            builder.AddProvider(new InMemoryCachedSecretProvider(cacheConfiguration), options => options.Name = secretProviderName);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="InMemorySecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secretName">The required secret name to store the secret in-memory.</param>
+        /// <param name="secretValue">The tested secret value to retrieve upon providing the <paramref name="secretName"/>.</param>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="secretName"/> is blank.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder, string secretName, string secretValue)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNullOrWhitespace(secretName, nameof(secretName), "Requires a non-blank test secret name to store the secret value in-memory");
+            
+            builder.AddInMemory(secretName, secretValue, secretProviderName: null);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemoryCachedSecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secretName">The required secret name to store the secret in-memory.</param>
+        /// <param name="secretValue">The tested secret value to retrieve upon providing the <paramref name="secretName"/>.</param>
+        /// <param name="cacheConfiguration">The <see cref="ICacheConfiguration" /> which defines how the cache works.</param>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="secretName"/> is blank.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or the <paramref name="cacheConfiguration"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(
+            this SecretStoreBuilder builder, 
+            string secretName, 
+            string secretValue, 
+            ICacheConfiguration cacheConfiguration)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNullOrWhitespace(secretName, nameof(secretName), "Requires a non-blank test secret name to store the secret value in-memory");
+            Guard.NotNull(cacheConfiguration, nameof(cacheConfiguration), "Requires a configuration instance to describe how the caching of secrets should work");
+
+            builder.AddInMemory(secretName, secretValue, cacheConfiguration, secretProviderName: null);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemorySecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secretName">The required secret name to store the secret in-memory.</param>
+        /// <param name="secretValue">The tested secret value to retrieve upon providing the <paramref name="secretName"/>.</param>
+        /// <param name="secretProviderName">The name to register the secret provider by.</param>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="secretName"/> is blank.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder, string secretName, string secretValue, string secretProviderName)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNullOrWhitespace(secretName, nameof(secretName), "Requires a non-blank test secret name to store the secret value in-memory");
+            
+            builder.AddProvider(new InMemorySecretProvider(secretName, secretValue), options => options.Name = secretProviderName);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemoryCachedSecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secretName">The required secret name to store the secret in-memory.</param>
+        /// <param name="secretValue">The tested secret value to retrieve upon providing the <paramref name="secretName"/>.</param>
+        /// <param name="cacheConfiguration">The <see cref="ICacheConfiguration" /> which defines how the cache works.</param>
+        /// <param name="secretProviderName">The name to register the secret provider by.</param>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="secretName"/> is blank.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or the <paramref name="cacheConfiguration"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(
+            this SecretStoreBuilder builder, 
+            string secretName, 
+            string secretValue, 
+            ICacheConfiguration cacheConfiguration,
+            string secretProviderName)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNullOrWhitespace(secretName, nameof(secretName), "Requires a non-blank test secret name to store the secret value in-memory");
+            Guard.NotNull(cacheConfiguration, nameof(cacheConfiguration), "Requires a configuration instance to describe how the caching of secrets should work");
+
+            builder.AddProvider(new InMemoryCachedSecretProvider(secretName, secretValue, cacheConfiguration), options => options.Name = secretProviderName);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemorySecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secrets">The set of secret name-value pairs that represents the complete set of available in-memory secrets.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder, IDictionary<string, string> secrets)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNull(secrets, nameof(secrets), "Requires a set of secrets to initialize the in-memory secret provider");
+            
+            builder.AddInMemory(secrets, secretProviderName: null);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemoryCachedSecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secrets">The set of secret name-value pairs that represents the complete set of available in-memory secrets.</param>
+        /// <param name="cacheConfiguration">The <see cref="ICacheConfiguration" /> which defines how the cache works.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the <paramref name="builder"/>, the <paramref name="secrets"/>, or the <paramref name="cacheConfiguration"/> is <c>null</c>.
+        /// </exception>
+        public static SecretStoreBuilder AddInMemory(
+            this SecretStoreBuilder builder, 
+            IDictionary<string, string> secrets, 
+            ICacheConfiguration cacheConfiguration)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNull(secrets, nameof(secrets), "Requires a set of secrets to initialize the in-memory secret provider");
+            Guard.NotNull(cacheConfiguration, nameof(cacheConfiguration), "Requires a configuration instance to describe how the caching of secrets should work");
+            
+            builder.AddInMemory(secrets, cacheConfiguration, secretProviderName: null);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemorySecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secrets">The set of secret name-value pairs that represents the complete set of available in-memory secrets.</param>
+        /// <param name="secretProviderName">The name to register the secret provider by.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="builder"/> or the <paramref name="secrets"/> is <c>null</c>.</exception>
+        public static SecretStoreBuilder AddInMemory(this SecretStoreBuilder builder, IDictionary<string, string> secrets, string secretProviderName)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNull(secrets, nameof(secrets), "Requires a set of secrets to initialize the in-memory secret provider");
+            
+            builder.AddProvider(new InMemorySecretProvider(secrets), options => options.Name = secretProviderName);
+            return builder;
+        }
+        
+        /// <summary>
+        /// Adds the <see cref="InMemoryCachedSecretProvider"/> to the secret store without any in-memory stored secrets.
+        /// </summary>
+        /// <param name="builder">The secret store builder to add the secret provider.</param>
+        /// <param name="secrets">The set of secret name-value pairs that represents the complete set of available in-memory secrets.</param>
+        /// <param name="cacheConfiguration">The <see cref="ICacheConfiguration" /> which defines how the cache works.</param>
+        /// <param name="secretProviderName">The name to register the secret provider by.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when the <paramref name="builder"/>, the <paramref name="secrets"/>, or the <paramref name="cacheConfiguration"/> is <c>null</c>.
+        /// </exception>
+        public static SecretStoreBuilder AddInMemory(
+            this SecretStoreBuilder builder, 
+            IDictionary<string, string> secrets, 
+            ICacheConfiguration cacheConfiguration,
+            string secretProviderName)
+        {
+            Guard.NotNull(builder, nameof(builder), "Requires a secret store builder to add the in-memory secret provider to the secret store");
+            Guard.NotNull(secrets, nameof(secrets), "Requires a set of secrets to initialize the in-memory secret provider");
+            Guard.NotNull(cacheConfiguration, nameof(cacheConfiguration), "Requires a configuration instance to describe how the caching of secrets should work");
+
+            builder.AddProvider(new InMemoryCachedSecretProvider(secrets, cacheConfiguration), options => options.Name = secretProviderName);
+            return builder;
+        }
+    }
+}
