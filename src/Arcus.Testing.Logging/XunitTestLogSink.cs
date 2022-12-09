4d59913541ecfddc60e4c4a3dab40663919bc377
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-12-09 10:17:43 +0100

    feat: add serilog xunit test log sink

---
+++ src/Arcus.Testing.Logging/XunitTestLogSink.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GuardNet;
+using Serilog.Core;
+using Serilog.Events;
+using Xunit.Abstractions;
+
+namespace Arcus.Testing.Logging
+{
+    /// <summary>
+    /// <see cref="ILogEventSink"/> representation of an <see cref="ITestOutputHelper"/> instance.
+    /// </summary>
+    public class XunitLogEventSink : ILogEventSink
+    {
+        private readonly ITestOutputHelper _outputWriter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XunitLogEventSink"/> class.
+        /// </summary>
+        /// <param name="outputWriter">The xUnit test output writer to write custom test output.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="outputWriter"/> is <c>null</c>.</exception>
+        public XunitLogEventSink(ITestOutputHelper outputWriter)
+        {
+            Guard.NotNull(outputWriter, nameof(outputWriter), "Requires a xUnit test output writer to write Serilog log messages to the xUnit test output");
+            _outputWriter = outputWriter;
+        }
+
+        /// <summary>
+        /// Emit the provided log event to the sink.
+        /// </summary>
+        /// <param name="logEvent">The log event to write.</param>
+        public void Emit(LogEvent logEvent)
+        {
+            _outputWriter.WriteLine(logEvent.RenderMessage());
+        }
+    }
+}
---
+++ src/Arcus.Testing.Tests.Unit/Logging/LoggerSinkConfigurationExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Arcus.Testing.Logging.Extensions;
+using Serilog;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Arcus.Testing.Tests.Unit.Logging
+{
+    public class LoggerSinkConfigurationExtensionsTests : ITestOutputHelper
+    {
+        private readonly ICollection<string> _messages = new Collection<string>();
+
+        [Fact]
+        public void AddXunitTestLogging_WithXunitOutputWriter_Succeeds()
+        {
+            // Arrange
+            var config = new LoggerConfiguration();
+            
+            // Act
+            config.WriteTo.AddXunitTestLogging(this);
+
+            // Assert
+            ILogger logger = config.CreateLogger();
+            var expected = "This information message should be present in the xUnit test output writer";
+            logger.Information(expected);
+            Assert.Single(_messages, expected);
+        }
+
+        [Fact]
+        public void AddXunitTestLogging_WithoutOutputWriter_Fails()
+        {
+            // Arrange
+            var config = new LoggerConfiguration();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(
+                () => config.WriteTo.AddXunitTestLogging(outputWriter: null));
+        }
+
+        public void WriteLine(string message)
+        {
+            _messages.Add(message);
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            _messages.Add(string.Format(format, args));
+        }
+    }
+}
