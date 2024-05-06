a0ebb7376ecb056ecf9bfa82aab5189401e84515
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2024-05-06 18:09:42 +0200

    pr-fix: make csv exception serializable + correct base func

--- src/Arcus.Testing.Assert/Failure/CsvException.cs
+++ src/Arcus.Testing.Assert/Failure/CsvException.cs
@@ -5,6 +5,7 @@ namespace Arcus.Testing.Failure
     /// <summary>
     /// Represents an exception that is thrown when invalid CSV is encountered.
     /// </summary>
+    [Serializable]
     public class CsvException : Exception
     {
         /// <summary>
@@ -27,7 +28,7 @@ namespace Arcus.Testing.Failure
         /// </summary>
         /// <param name="message">The message that describes the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception.</param>
-        public CsvException(string message, Exception innerException)
+        public CsvException(string message, Exception innerException) : base(message, innerException)
         {
         }
     }
