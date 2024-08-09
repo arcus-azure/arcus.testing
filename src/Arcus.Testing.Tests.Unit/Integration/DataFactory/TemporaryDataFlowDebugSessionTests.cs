8ad8b91d2b098ec2c5f4e8ad35372b1578b05041
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2024-08-09 07:31:27 +0200

    pr-fix: correct options testing

--- src/Arcus.Testing.Tests.Unit/Integration/DataFactory/TemporaryDataFlowDebugSessionTests.cs
+++ src/Arcus.Testing.Tests.Unit/Integration/DataFactory/TemporaryDataFlowDebugSessionTests.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
-using Azure;
-using Azure.Core;
-using Azure.ResourceManager;
+using Arcus.Testing.Tests.Unit.Integration.DataFactory.Fixture;
 using Azure.ResourceManager.DataFactory;
-using Azure.ResourceManager.DataFactory.Models;
+using Bogus;
 using Microsoft.Extensions.Logging;
-using Moq;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Arcus.Testing.Tests.Unit.Integration.DataFactory
 {
-    public class TemporaryDataFlowDebugSessionTests
+    public partial class TemporaryDataFlowDebugSessionTests
     {
         private readonly ILogger _logger;
+        private static readonly Faker Bogus = new();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TemporaryDataFlowDebugSessionTests" /> class.
@@ -33,45 +29,38 @@ namespace Arcus.Testing.Tests.Unit.Integration.DataFactory
             var spyResource = new StubDataFactoryResource();
 
             // Act
-            var session = await TemporaryDataFlowDebugSession.StartDebugSessionAsync(spyResource, _logger);
+            TemporaryDataFlowDebugSession session = await StartDebugSessionAsync(spyResource);
 
             // Assert
             Assert.True(spyResource.IsActive, "DataFlow debug session should be active after starting test fixture");
+            Assert.Equal(spyResource.SessionId, session.SessionId);
+
             await session.DisposeAsync();
             Assert.False(spyResource.IsActive, "DataFlow debug session should be inactive after disposing test fixture");
         }
-    }
 
-    public class StubDataFactoryResource : DataFactoryResource
-    {
-        public bool IsActive { get; private set; }
-
-        public override DataFactoryData Data { get; } = new(AzureLocation.WestEurope);
-
-        public override Task<ArmOperation<DataFactoryDataFlowCreateDebugSessionResult>> CreateDataFlowDebugSessionAsync(
-            WaitUntil waitUntil,
-            DataFactoryDataFlowDebugSessionContent content,
-            CancellationToken cancellationToken = new CancellationToken())
+        private async Task<TemporaryDataFlowDebugSession> StartDebugSessionAsync(DataFactoryResource resource, Action<TemporaryDataFlowDebugSessionOptions> configureOptions = null)
         {
-            var result = new Mock<DataFactoryDataFlowCreateDebugSessionResult>(
-                "status",
-                Guid.NewGuid(),
-                new Dictionary<string, BinaryData>());
-
-            var operation = new Mock<ArmOperation<DataFactoryDataFlowCreateDebugSessionResult>>();
-            operation.Setup(r => r.Value)
-                     .Returns(result.Object);
-
-            IsActive = true;
-            return Task.FromResult(operation.Object);
+            return configureOptions is null
+                ? await TemporaryDataFlowDebugSession.StartDebugSessionAsync(resource, _logger)
+                : await TemporaryDataFlowDebugSession.StartDebugSessionAsync(resource, _logger, configureOptions);
         }
 
-        public override Task<Response> DeleteDataFlowDebugSessionAsync(
-            DeleteDataFlowDebugSessionContent content,
-            CancellationToken cancellationToken = new CancellationToken())
+        [Fact]
+        public async Task StartDebugSession_WithoutDataFactoryResource_Fails()
         {
-            IsActive = false;
-            return Task.FromResult(Mock.Of<Response>());
+            await Assert.ThrowsAnyAsync<ArgumentException>(() => StartDebugSessionAsync(resource: null));
+            await Assert.ThrowsAnyAsync<ArgumentException>(() => StartDebugSessionAsync(resource: null, configureOptions: opt => { }));
+        }
+
+        [Fact]
+        public void TimeToLive_WithNegativeValue_Fails()
+        {
+            // Arrange
+            var options = new TemporaryDataFlowDebugSessionOptions();
+
+            // Act / Assert
+            Assert.ThrowsAny<ArgumentException>(() => options.TimeToLiveInMinutes = Bogus.Random.Int(max: -1));
         }
     }
 }
