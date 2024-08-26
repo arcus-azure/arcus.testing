c06ce7da503a7ef377f2e274998d82b2a6677f82
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2024-08-26 12:10:18 +0200

    pr-fix: correct items

--- build/templates/policy-tests.ps1
+++ build/templates/policy-tests.ps1
@@ -4,8 +4,11 @@ $items = az policy state list -g 'arcus-testing-dev-we-rg' | ConvertFrom-Json
 Describe 'Azure policies' {
   foreach ($item in $items) {
     It $item.policyDefinitionReferenceId {
+      Write-Host $item
+      Write-Host $definitions
       $definition = $definitions | where { $_.id -eq $item.policyDefinitionId }
-      $item.complianceState | Should -Be 'Compliant' -Because ($definition.description + $items)
+      Write-Host $definition
+      $item.complianceState | Should -Be 'Compliant' -Because $definition.description
     }
   }
 }
