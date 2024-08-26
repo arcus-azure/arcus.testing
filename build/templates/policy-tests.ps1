1f935c19efba6baf7a32a82f80bfc6aaae90ce1b
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2024-08-26 11:55:12 +0200

    pr-fix: correct az policy tests

---
+++ build/templates/policy-tests.ps1
@@ -0,0 +1,11 @@
+$definitions = az policy definition list | ConvertFrom-Json
+$items = az policy state list -g 'arcus-testing-dev-we-rg' | ConvertFrom-Json
+
+Describe 'Azure policies' {
+  foreach ($item in $items) {
+    It $item.policyDefinitionReferenceId {
+      $definition = $definitions | where { $_.id -eq $item.policyDefinitionId }
+      $item.complianceState | Should -Be 'Compliant' -Because $definition.description
+    }
+  }
+}
