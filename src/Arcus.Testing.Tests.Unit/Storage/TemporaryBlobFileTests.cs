e577f639d65b9da135f853ac43a5efb16acb11da
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2024-09-25 06:22:06 +0200

    feat: expose `OnTeardown` for blob infra (#186)
    * pr-fix: correct merge w/ 'main'
    
    * feat: expose OnTeardown for blob test infra

---
+++ src/Arcus.Testing.Tests.Unit/Storage/TemporaryBlobFileTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+namespace Arcus.Testing.Tests.Unit.Storage
+{
+    public class TemporaryBlobFileTests
+    {
+        [Fact]
+        public async Task UploadBlobFileIfNotExists_WithoutContainerUri_Fails()
+        {
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    blobContainerUri: null,
+                    "<blob-name>",
+                    BinaryData.FromString("<blob-content>"),
+                    NullLogger.Instance));
+
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    blobContainerUri: null,
+                    "<blob-name>",
+                    BinaryData.FromString("<blob-content>"),
+                    NullLogger.Instance,
+                    configureOptions: opt => { }));
+        }
+
+        [Theory]
+        [ClassData(typeof(Blanks))]
+        public async Task UploadBlobFileIfNotExistsViaContainerUri_WithoutBlobName_Fails(string blobName)
+        {
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    new Uri("https://some-url"),
+                    blobName,
+                    BinaryData.FromString("<blob-content>"),
+                    NullLogger.Instance));
+
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    new Uri("https://some-url"),
+                    blobName,
+                    BinaryData.FromString("<blob-content>"),
+                    NullLogger.Instance,
+                    configureOptions: opt => { }));
+        }
+
+        [Fact]
+        public async Task UploadBlobFileIfNotExistsViaContainerUri_WithoutBlobContent_Fails()
+        {
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    new Uri("https://some-url"),
+                    "<blob-name>",
+                    blobContent: null,
+                    NullLogger.Instance));
+
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    new Uri("https://some-url"),
+                    "<blob-name>",
+                    blobContent: null,
+                    NullLogger.Instance,
+                    configureOptions: opt => { }));
+        }
+
+        [Fact]
+        public async Task UploadBlobFileIfNotExists_WithoutBlobClient_Fails()
+        {
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    blobClient: null,
+                    BinaryData.FromString("<blob-content>"),
+                    NullLogger.Instance));
+
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    blobClient: null,
+                    BinaryData.FromString("<blob-content>"),
+                    NullLogger.Instance,
+                    configureOptions: opt => { }));
+        }
+
+        [Fact]
+        public async Task UploadBlobFileIfNotExistsViaBlobClient_WithoutBlobContent_Fails()
+        {
+            var client = Mock.Of<BlobClient>();
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    client,
+                    blobContent: null,
+                    NullLogger.Instance));
+
+            await Assert.ThrowsAnyAsync<ArgumentException>(
+                () => TemporaryBlobFile.UploadIfNotExistsAsync(
+                    client,
+                    blobContent: null,
+                    NullLogger.Instance,
+                    opt => { }));
+        }
+    }
+}
