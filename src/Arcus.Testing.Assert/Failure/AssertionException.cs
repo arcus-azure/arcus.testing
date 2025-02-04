e1570666c68f6a13ceabe3817e602aff733c1ceb
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-02-04 18:40:58 +0100

    fix: add exception serialization constructors

--- src/Arcus.Testing.Assert/Failure/AssertionException.cs
+++ src/Arcus.Testing.Assert/Failure/AssertionException.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 // ReSharper disable once CheckNamespace - place the exceptions in the root namespace for less clutter when exception is written to test output.
 namespace Arcus.Testing
@@ -27,6 +28,18 @@ namespace Arcus.Testing
         /// Initializes a new instance of the <see cref="AssertionException" /> class.
         /// </summary>
         public AssertionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssertionException" /> class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="info" /> is <see langword="null" />.</exception>
+        /// <exception cref="SerializationException">Thrown when the class name is <see langword="null" /> or <see cref="Exception.HResult" /> is zero (0).</exception>
+        protected AssertionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
--- src/Arcus.Testing.Assert/Failure/CsvException.cs
+++ src/Arcus.Testing.Assert/Failure/CsvException.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Arcus.Testing.Failure
 {
@@ -29,6 +30,18 @@ namespace Arcus.Testing.Failure
         /// <param name="message">The message that describes the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception.</param>
         public CsvException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CsvException" /> class with serialized data.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext" /> that contains contextual information about the source or destination.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="info" /> is <see langword="null" />.</exception>
+        /// <exception cref="SerializationException">Thrown when the class name is <see langword="null" /> or <see cref="Exception.HResult" /> is zero (0).</exception>
+        protected CsvException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
