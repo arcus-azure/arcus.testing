da30ed46564282fcb99f8f1e936f772c8e0e8626
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-03-04 08:49:40 +0100

    chore(table): remove temp table entity filter

--- src/Arcus.Testing.Tests.Unit/Storage/TemporaryTableTests.cs
+++ src/Arcus.Testing.Tests.Unit/Storage/TemporaryTableTests.cs
@@ -1,19 +1,14 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Azure.Data.Tables;
-using Bogus;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Xunit;
-
-#pragma warning disable CS0618 // Ignore obsolete warnings that we added ourselves, should be removed upon releasing v2.0.
 
 namespace Arcus.Testing.Tests.Unit.Storage
 {
     public class TemporaryTableTests
     {
-        private static readonly Faker Bogus = new();
-
         [Fact]
         public async Task CreateTempTable_WithoutClient_Fails()
         {
@@ -37,8 +32,6 @@ namespace Arcus.Testing.Tests.Unit.Storage
 
             // Act / Assert
             Assert.ThrowsAny<ArgumentException>(() => options.OnSetup.CleanMatchingEntities((Func<TableEntity, bool>) null));
-            Assert.ThrowsAny<ArgumentException>(() => options.OnSetup.CleanMatchingEntities((TableEntityFilter) null));
-            Assert.ThrowsAny<ArgumentException>(() => options.OnSetup.CleanMatchingEntities(TableEntityFilter.RowKeyEqual("<row-key>"), null));
         }
 
         [Fact]
@@ -49,30 +42,6 @@ namespace Arcus.Testing.Tests.Unit.Storage
 
             // Act / Assert
             Assert.ThrowsAny<ArgumentException>(() => options.OnTeardown.CleanMatchingEntities((Func<TableEntity, bool>) null));
-            Assert.ThrowsAny<ArgumentException>(() => options.OnTeardown.CleanMatchingEntities((TableEntityFilter) null));
-            Assert.ThrowsAny<ArgumentException>(() => options.OnTeardown.CleanMatchingEntities(TableEntityFilter.PartitionKeyEqual("<partition-key>"), null));
-        }
-
-        [Theory]
-        [ClassData(typeof(Blanks))]
-        public void RowKeyEqual_WithoutRowKey_Fails(string rowKey)
-        {
-            Assert.ThrowsAny<ArgumentException>(() => TableEntityFilter.RowKeyEqual(rowKey));
-            Assert.ThrowsAny<ArgumentException>(() => TableEntityFilter.RowKeyEqual(rowKey, Bogus.PickRandom<StringComparison>()));
-        }
-
-        [Theory]
-        [ClassData(typeof(Blanks))]
-        public void PartitionKeyEqual_WithoutPartitionKey_Fails(string partitionKey)
-        {
-            Assert.ThrowsAny<ArgumentException>(() => TableEntityFilter.PartitionKeyEqual(partitionKey));
-            Assert.ThrowsAny<ArgumentException>(() => TableEntityFilter.PartitionKeyEqual(partitionKey, Bogus.PickRandom<StringComparison>()));
-        }
-
-        [Fact]
-        public void EntityEqual_WithoutFilter_Fails()
-        {
-            Assert.ThrowsAny<ArgumentException>(() => TableEntityFilter.EntityEqual(null));
         }
     }
 }
