5d364fa0f9da617c6530ae36b0e4410795135a33
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-04-29 07:50:25 +0200

    temp: try setting context search to 'false'

--- docs/docusaurus.build.config.js
+++ docs/docusaurus.build.config.js
@@ -8,7 +8,7 @@ module.exports = {
       appId: process.env.ALGOLIA_APP_ID,
       apiKey: process.env.ALGOLIA_API_KEY,
       indexName: 'testing-arcus-azure',
-      // Set `contextualSearch` to `true` when having multiple versions!!!
+      // Set `contextualSearch` to `true` when having multiple versions!
       contextualSearch: true,
       searchParameters: {
         facetFilters: ['tags:testing'],
