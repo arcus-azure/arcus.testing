faec7d7d208b3a34ba6a170b52450736b42e66c3
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-04-29 07:35:00 +0200

    temp: try setting context search to 'false'

--- docs/docusaurus.build.config.js
+++ docs/docusaurus.build.config.js
@@ -5,8 +5,8 @@ module.exports = {
   themeConfig: {
     ...buildConfig.themeConfig,
     algolia: {
-      appId: 'HTBVPW3ZI8',
-      apiKey: 'dad662ed8cb6d18cfdfe8767fb742516',
+      appId: process.env.ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_API_KEY,
       indexName: 'testing-arcus-azure',
       // Set `contextualSearch` to `true` when having multiple versions!!!
       contextualSearch: true,
