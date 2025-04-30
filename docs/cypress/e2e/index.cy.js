1a2f39d8d94e86e5e06fd9be4341552dce4047e7
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2025-04-30 12:34:31 +0200

    test(docs): add algolia search tests

--- docs/cypress/e2e/index.cy.js
+++ docs/cypress/e2e/index.cy.js
@@ -1,6 +1,25 @@
 describe('Index page', () => {
-  it('loads', () => {
+  beforeEach(() =>{
     cy.visit('/')
+  })
+
+  it('loads', () => {
     cy.contains('h1', 'Introduction').should('be.visible')
+  })
+
+  it('has the Algolia search bar', () => {
+    cy.window().then((win) => {
+      expect(win.docsearch).to.exist
+    })
+  })
+
+  it('opens the Algolia search modal when typing in the search input', () => {
+    cy.get('input[placeholder="Search"]').type('intro')
+    cy.get('.DocSearch-Modal').should('exist')
+  })
+
+  it('shows results in the Algolia search dropdown', () => {
+    cy.get('input[placeholder="Search"]').type('intro')
+    cy.get('.DocSearch-Hit').should('have.length.at.least', 1)
   })
 })
