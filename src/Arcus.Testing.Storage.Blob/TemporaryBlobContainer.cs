b7064a887e669a97325dc69fceb35983fa129458
9039753+stijnmoreels@users.noreply.github.com <9039753+stijnmoreels@users.noreply.github.com>
2025-05-01 08:46:42 +0200

    chore(blob): introduce 'upsert blob' renaming

--- src/Arcus.Testing.Storage.Blob/TemporaryBlobContainer.cs
+++ src/Arcus.Testing.Storage.Blob/TemporaryBlobContainer.cs
@@ -18,7 +18,7 @@ namespace Arcus.Testing
     /// <summary>
     /// Represents the available options when the <see cref="TemporaryBlobContainer"/> is cleaned.
     /// </summary>
-    internal enum OnTeardownBlobs { CleanIfCreated = 0, CleanAll = 1, CleanIfMatched = 2 }
+    internal enum OnTeardownBlobs { CleanIfUpserted = 0, CleanAll = 1, CleanIfMatched = 2 }
 
     /// <summary>
     /// Represents the available options when the <see cref="TemporaryBlobContainer"/> is deleted.
@@ -106,7 +106,7 @@ namespace Arcus.Testing
         /// <summary>
         /// Gets the configurable option on what to do with unlinked Azure Blobs in the Azure Blob container upon the disposal of the test fixture.
         /// </summary>
-        internal OnTeardownBlobs Blobs { get; private set; } = OnTeardownBlobs.CleanIfCreated;
+        internal OnTeardownBlobs Blobs { get; private set; } = OnTeardownBlobs.CleanIfUpserted;
 
         /// <summary>
         /// Gets the configurable option on what to do with the Azure Blob container upon the disposal of the test fixture.
@@ -115,11 +115,21 @@ namespace Arcus.Testing
 
         /// <summary>
         /// (default for cleaning blobs) Configures the <see cref="TemporaryBlobContainer"/> to only delete the Azure Blobs upon disposal
-        /// if the blob was uploaded by the test fixture (using <see cref="TemporaryBlobContainer.UploadBlobAsync(string, BinaryData)"/>).
+        /// if the blob was upserted by the test fixture (using <see cref="TemporaryBlobContainer.UpsertBlobFileAsync(string, BinaryData)"/>).
         /// </summary>
+        [Obsolete("Will be removed in v3, please use " + nameof(CleanUpsertedBlobs) + " instead that provides exactly the same on-teardown functionality")]
         public OnTeardownBlobContainerOptions CleanCreatedBlobs()
         {
-            Blobs = OnTeardownBlobs.CleanIfCreated;
+            return CleanUpsertedBlobs();
+        }
+
+        /// <summary>
+        /// (default for cleaning blobs) Configures the <see cref="TemporaryBlobContainer"/> to only delete the Azure Blobs upon disposal
+        /// if the blob was upserted by the test fixture (using <see cref="TemporaryBlobContainer.UpsertBlobFileAsync(string, BinaryData)"/>).
+        /// </summary>
+        public OnTeardownBlobContainerOptions CleanUpsertedBlobs()
+        {
+            Blobs = OnTeardownBlobs.CleanIfUpserted;
             return this;
         }
 
@@ -207,7 +217,7 @@ namespace Arcus.Testing
         /// <summary>
         /// Gets the additional options to manipulate the deletion of the <see cref="TemporaryBlobContainer"/>.
         /// </summary>
-        public OnTeardownBlobContainerOptions OnTeardown { get; } = new OnTeardownBlobContainerOptions().CleanCreatedBlobs().DeleteCreatedContainer();
+        public OnTeardownBlobContainerOptions OnTeardown { get; } = new OnTeardownBlobContainerOptions().CleanUpsertedBlobs().DeleteCreatedContainer();
     }
 
     /// <summary>
@@ -345,7 +355,24 @@ namespace Arcus.Testing
         /// <param name="blobContent">The content of the blob to upload.</param>
         /// <exception cref="ArgumentException">Thrown when the <paramref name="blobName"/> is blank.</exception>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="blobContent"/> is <c>null</c>.</exception>
+        [Obsolete("Will be removed in v3, please use the " + nameof(UpsertBlobFileAsync) + " instead that provides exactly the same functionality")]
         public async Task<BlobClient> UploadBlobAsync(string blobName, BinaryData blobContent)
+        {
+            return await UpsertBlobFileAsync(blobName, blobContent);
+        }
+
+        /// <summary>
+        /// Uploads a new or replaces an existing blob in the Azure Blob container (a.k.a. UPSERT).
+        /// </summary>
+        /// <remarks>
+        ///     Any blob files upserted via this call will always be deleted (if new) or reverted (if existing)
+        ///     when the <see cref="TemporaryBlobContainer"/> is disposed.
+        /// </remarks>
+        /// <param name="blobName">The name of the blob to upload.</param>
+        /// <param name="blobContent">The content of the blob to upload.</param>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="blobName"/> is blank.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="blobContent"/> is <c>null</c>.</exception>
+        public async Task<BlobClient> UpsertBlobFileAsync(string blobName, BinaryData blobContent)
         {
             ArgumentException.ThrowIfNullOrWhiteSpace(blobName);
             ArgumentNullException.ThrowIfNull(blobContent);
@@ -403,7 +430,7 @@ namespace Arcus.Testing
 
         private static async Task CleanBlobContainerUponDeletionAsync(BlobContainerClient containerClient, TemporaryBlobContainerOptions options, ILogger logger)
         {
-            if (options.OnTeardown.Blobs is OnTeardownBlobs.CleanIfCreated)
+            if (options.OnTeardown.Blobs is OnTeardownBlobs.CleanIfUpserted)
             {
                 return;
             }
