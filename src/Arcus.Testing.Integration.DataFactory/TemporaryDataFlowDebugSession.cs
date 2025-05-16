fb4e46396498e7c5a8793c90fbb7233a010d2e30
Clément Vaillant <clement.vaillant@codit.eu>
2025-05-16 10:29:16 +0200

    Update src/Arcus.Testing.Integration.DataFactory/TemporaryDataFlowDebugSession.cs
    Co-authored-by: Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>

--- src/Arcus.Testing.Integration.DataFactory/TemporaryDataFlowDebugSession.cs
+++ src/Arcus.Testing.Integration.DataFactory/TemporaryDataFlowDebugSession.cs
@@ -445,7 +445,7 @@ namespace Arcus.Testing
         /// </summary>
         /// <remarks>
         ///     <para>For string parameters, the value must be enclosed in single quotes (example: <c>"'myValue'"</c>).</para>
-        ///     <para>For boolean parameters, the value must be either <c>"true()"</c> or <c>"false()"</c>.</para
+        ///     <para>For boolean parameters, the value must be either <c>"true()"</c> or <c>"false()"</c>.</para>
         /// </remarks>
         /// <exception cref="ArgumentException">Thrown when the <paramref name="name"/> is blank.</exception>
         /// <exception cref="ArgumentNullException">Thrown when the <paramref name="value"/> is null.</exception>
