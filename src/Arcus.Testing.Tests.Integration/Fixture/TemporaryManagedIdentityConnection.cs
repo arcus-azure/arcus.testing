4bc2ce92bc0cfb5d99262da04fa24f51956bf019
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2025-09-25 09:57:28 +0200

    fix(test.auth): move managed identity as an assembly test fixture (#479)
    Due to the full parallelization of the integration tests (see #476), we
    need to make sure that there are no shared test fixtures across the
    tests. It turns out that we have configured a managed identity
    connection for each test context, and since this is pointing to the same
    service principal, it could happen that it gets teared down while
    another test still needs it.
    
    This PR moves the managed identity test fixture to an assembly test
    fixture, so that authentication is configured once and shared across the
    tests.

--- src/Arcus.Testing.Tests.Integration/Fixture/TemporaryManagedIdentityConnection.cs
+++ src/Arcus.Testing.Tests.Integration/Fixture/TemporaryManagedIdentityConnection.cs
@@ -1,45 +1,37 @@
 ﻿using System;
 using Arcus.Testing.Tests.Integration.Configuration;
-using Microsoft.Extensions.Logging;
+using Arcus.Testing.Tests.Integration.Fixture;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
+
+[assembly: AssemblyFixture(typeof(TemporaryManagedIdentityConnection))]
 
 namespace Arcus.Testing.Tests.Integration.Fixture
 {
     /// <summary>
     /// Represents a temporary managed identity authentication that is set for the duration of the test.
     /// </summary>
-    internal sealed class TemporaryManagedIdentityConnection : IDisposable
+    public sealed class TemporaryManagedIdentityConnection : IDisposable
     {
         private readonly TemporaryEnvironmentVariable[] _environmentVariables;
 
-        private TemporaryManagedIdentityConnection(TemporaryEnvironmentVariable[] environmentVariables)
-        {
-            _environmentVariables = environmentVariables;
-        }
-
         /// <summary>
-        /// Creates a new <see cref="TemporaryManagedIdentityConnection"/> instance for a specific <paramref name="configuration"/>
-        /// using the current registration of the service principal.
+        /// Initializes a new instance of the <see cref="TemporaryManagedIdentityConnection"/> class.
         /// </summary>
-        public static TemporaryManagedIdentityConnection Create(TestConfig configuration, ILogger logger)
+        public TemporaryManagedIdentityConnection()
         {
-            ArgumentNullException.ThrowIfNull(configuration);
-            logger ??= NullLogger.Instance;
+            var configuration = TestConfig.Create();
+            var logger = NullLogger.Instance;
 
             ServicePrincipal servicePrincipal = configuration.GetServicePrincipal();
-            if (servicePrincipal.IsDefault)
-            {
-                logger.LogTrace("[Test:Setup] no local service principal was registered in the test configuration 'appsettings.*.json', which means managed identity relies on local authenticated sessions (ex. VisualStudio account, Azure CLI...)");
-                return new TemporaryManagedIdentityConnection([]);
-            }
-
-            return new TemporaryManagedIdentityConnection(
-            [
-                TemporaryEnvironmentVariable.SetSecretIfNotExists("AZURE_TENANT_ID", servicePrincipal.TenantId, logger),
-                TemporaryEnvironmentVariable.SetSecretIfNotExists("AZURE_CLIENT_ID", servicePrincipal.ClientId, logger),
-                TemporaryEnvironmentVariable.SetSecretIfNotExists("AZURE_CLIENT_SECRET", servicePrincipal.ClientSecret, logger)
-            ]);
+            _environmentVariables = servicePrincipal.IsDefault
+                ? []
+                :
+                [
+                    TemporaryEnvironmentVariable.SetSecretIfNotExists("AZURE_TENANT_ID", servicePrincipal.TenantId, logger),
+                    TemporaryEnvironmentVariable.SetSecretIfNotExists("AZURE_CLIENT_ID", servicePrincipal.ClientId, logger),
+                    TemporaryEnvironmentVariable.SetSecretIfNotExists("AZURE_CLIENT_SECRET", servicePrincipal.ClientSecret, logger)
+                ];
         }
 
         /// <summary>
